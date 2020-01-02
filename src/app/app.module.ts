d0c9754faa462ae0fff39773a63da9877ee392ff
Satish Hingu <Satish.Hingu@INFOVISIONLABS.COM>
2020-01-02 16:53:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProductComponent } from './product/product.component';
+import { ApiComponent } from './api/api.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ItemComponent } from './item/item.component';
+import {  FormsModule,  ReactiveFormsModule  } from '@angular/forms';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    SidebarComponent,
+    ProductComponent,
+    ApiComponent,
+    ItemComponent,
+    ItemListComponent,
+         ],
+  entryComponents: [],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
